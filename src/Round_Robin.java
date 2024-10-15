5144ef74d14fcdc61005b414d8b9fb9073a960fa
Parimal Matte <pmprofessional26@gmail.com>
2024-10-15 23:15:55 +0530

    SJF Scheduling commit

--- src/Round_Robin.java
+++ src/Round_Robin.java
@@ -6,8 +6,10 @@ public class Round_Robin {
     private static final Scanner sc = new Scanner(System.in);
     private static final ArrayList<Process> processes = new ArrayList<>();
 
+    private static int TIME_QUANTUM = 2;
+
     private static class Process implements Comparable<Process> {
-        int at, bt, ct, wt, tat, index, rt, tempBT, cpuFirstTime;
+        int at, bt, wt, tat, index, rt, tempBT, cpuFirstTime;
         boolean started = false;
         public Process(int at, int bt, int index){
             this.at = at;
@@ -30,7 +32,7 @@ public class Round_Robin {
         }
     }
 
-    private static void setProcesses(){
+    private static void getProcesses(){
         System.out.println("Enter the no.of processes: ");
         int n = sc.nextInt();
         for(int x = 0; x < n; x++){
@@ -43,6 +45,65 @@ public class Round_Robin {
         }
     }
 
+    private static void getProcessesToExecute(Queue<Process> readyQueue, ArrayList<Process> processes, int currentTime) {
+        processes.stream()
+                .filter(process -> process.tempBT > 0 && !readyQueue.contains(process) && process.at <= currentTime)
+                .sorted(Process::compareByAt)
+                .forEach(readyQueue::add);
+    }
+
+    private static boolean shouldStop(ArrayList<Process> processes){
+        return processes.stream()
+                .filter(process -> process.tempBT > 0)
+                .toList().isEmpty();
+    }
+
+    private static void performScheduling(){
+        processes.sort(Process::compareByAt);
+        int currentTime = 0;
+        List<Process> processesToExecute = processes;
+        Queue<Process> readyQueue = new LinkedList<>();
+        readyQueue.add(processesToExecute.get(0));
+        while(!shouldStop(processes)){
+            Process process = readyQueue.peek();
+            if(!process.started){
+                process.started = true;
+                process.cpuFirstTime = currentTime;
+            }
+            if(process.tempBT > TIME_QUANTUM){
+                currentTime += TIME_QUANTUM;
+                process.tempBT -= TIME_QUANTUM;
+            }
+            else{
+                currentTime += process.tempBT;
+                process.tempBT = 0;
+                process.rt = currentTime;
+            }
+            getProcessesToExecute(readyQueue, processes, currentTime);
+            readyQueue.remove();
+            if(process.tempBT > 0){
+                readyQueue.add(process);
+            }
+        }
+        for(Process process : processes){
+            process.tat = process.rt - process.at;
+            process.wt = process.tat - process.bt;
+        }
+    }
+
+    private static void showResults() {
+        System.out.println("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnAround Time\tCPU FirstTime\tResponse Time");
+        for (Process process : processes) {
+            System.out.println(process.index + "\t\t" + process.at + "\t\t\t\t" + process.bt + "\t\t\t\t" + process.wt + "\t\t\t\t" + process.tat + "\t\t\t\t" + process.cpuFirstTime + "\t\t\t\t" + process.rt);
+        }
+    }
+
+
+    public static void main(String[] args){
+        getProcesses();
+        performScheduling();
+        showResults();
+    }
 
 
 }
